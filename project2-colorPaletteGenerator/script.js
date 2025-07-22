aadd01cdeffcc5ddefe3ccd1d8568efad4a13d96
Muhammad Danial <danialashfaq0@gmail.com>
2025-07-22 14:13:52 +0500

    added copyin feature to the project 2

--- project2-colorPaletteGenerator/script.js
+++ project2-colorPaletteGenerator/script.js
@@ -3,6 +3,35 @@ const paletteContainer=document.querySelector(".palette-container");
 
 generateButton.addEventListener("click",generatePalette);
 
+paletteContainer.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("copy-button")){
+        const hexValue=e.target.previousElementSibling.textContent;
+
+        navigator.clipboard.writeText(hexValue).then(  ()=>showCopySuccess(e.target)).catch((e)=>{alert(e)});
+    }
+    else if(e.target.classList.contains("color")){
+        const hexValue=e.target.nextElementSibling.textContent;
+
+        navigator.clipboard.writeText(hexValue).then(  ()=>showCopySuccess(e.target.nextElementSibling.querySelector(".copy-button"))).catch((e)=>{alert(e)});
+    }
+});
+
+function showCopySuccess(e){
+    e.classList.remove("far"  ,"fa-copy");
+    e.classList.add("fas","fa-check");
+
+    e.style.color='#48bb78';
+
+    setTimeout(()=>{
+    e.classList.remove("fas","fa-check");
+    e.classList.add("far"  ,"fa-copy");
+
+    e.style.color="";
+
+
+    },1500);
+}
+
 function generatePalette(){
 
     const colors=[];
